b71904474e3cd49e97b76403d1ac80021d80c078
KoBBIBI <Patapob.c@gmail.com>
2021-01-26 11:02:27 +0700

    commit

--- ConnectDB.py
+++ ConnectDB.py
@@ -2,8 +2,10 @@ import psycopg2
 try:
     connection = psycopg2.connect(user="webadmin",
                                     password="EYHmqn42316",
-                                    host="node8545-advweb-01.app.ruk-com.cloud",
-                                    port="11085",
+                                    host="10.100.2.33",
+                                    port="5432"
+                                    # host="node8545-advweb-01.app.ruk-com.cloud",
+                                    # port="11085",
                                     database="CloudDB")
     cursor = connection.cursor()
     print(connection.get_dsn_parameters(),"\n")
